d91272051be3d1dccdab61210134ff26741c1dfc
Ian Gow <iandgow@gmail.com>
2018-08-31 08:05:56 +1000

    Removed references to bs_linguistics.

--- liwc_etc/liwc_create_word_list.R
+++ liwc_etc/liwc_create_word_list.R
@@ -1,6 +1,7 @@
 # Get LIWC table from Google Sheets
 library(googlesheets)
 
+target_schema <- "se_features"
 # As a one-time thing per user and machine, you will need to run gs_auth()
 # to authorize googlesheets to access your Google Sheets.
 key <- "17GGtdEyQI1ioAwlnjHBMV4rgANY7IQkiN_peC2i3llw"
@@ -21,27 +22,30 @@ df <- subset(df, !is.na(word))
 # Push data to PostgreSQL
 library(RPostgreSQL)
 pg <- dbConnect(PostgreSQL())
+rs <- dbExecute(pg, sprintf("SET search_path TO %s", target_schema))
 rs <- dbWriteTable(pg, "word_list_raw", df, overwrite=TRUE, row.names=FALSE)
 
 # Create a new table in PostgreSQL where words are aggregated into an
 # array of words for each category.
-rs <- dbGetQuery(pg, "
-    DROP TABLE IF EXISTS se_features.word_list;
 
-    CREATE TABLE se_features.word_list AS
+rs <- dbGetQuery(pg, sprintf("
+    DROP TABLE IF EXISTS word_list;
+
+    CREATE TABLE word_list AS
     SELECT category, array_agg(word) AS word_list
     FROM word_list_raw
     GROUP BY category;
 
     ALTER TABLE se_features.word_list OWNER TO se_features;
+    ALTER TABLE word_list OWNER TO %s;
 
-    DROP TABLE IF EXISTS word_list_raw;")
+    DROP TABLE IF EXISTS word_list_raw", target_schema))
 
 db_comment <- paste0("CREATED USING liwc_etc/",
                      "liwc_create_word_list.R and data in ",
                     "https://docs.google.com/spreadsheets/d/", key, " ON ",
                     Sys.time())
-dbGetQuery(pg, paste0("COMMENT ON TABLE se_features.word_list IS '",
+dbGetQuery(pg, paste0("COMMENT ON TABLE word_list IS '",
                       db_comment, "'"))
 
 rs <- dbDisconnect(pg)
--- liwc_etc/liwc_functions.py
+++ liwc_etc/liwc_functions.py
@@ -9,12 +9,14 @@ from pandas.io.json import json_normalize
 conn_string = 'postgresql://' + os.environ['PGHOST'] + '/' + os.environ['PGDATABASE']
 engine = create_engine(conn_string)
 
-rv = engine.execute("SELECT category FROM se_features.word_list")
+target_schema = "se_features"
+engine.execute("SET search_path TO se_features, public")
+rv = engine.execute("SELECT category FROM word_list")
 categories = [ (r["category"]) for r in rv]
 
 plan = """
     SELECT word_list
-    FROM se_features.word_list
+    FROM word_list
     WHERE category = %s"""
 
 mod_word_list = {}
