ad1e2df20d86131e7c34d7750a46d143da91c83f
Ian Gow <iandgow@gmail.com>
2018-09-06 21:23:15 -0400

    Added admin.sql.

---
+++ admin.sql
@@ -0,0 +1,6 @@
+-- In R, system("psql -f admin.sql") runs this
+CREATE SCHEMA se_features;
+CREATE ROLE se_features;
+CREATE ROLE se_features_access;
+ALTER SCHEMA se_features OWNER TO se_features;
+GRANT USAGE ON SCHEMA se_features TO se_features_access;
