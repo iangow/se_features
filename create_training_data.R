85049cd629fa05ee52ffb6ab4721ef4c5425c733
Ian Gow <iandgow@gmail.com>
2019-05-15 09:39:47 +1000

    Tweaks to training-data code.
    - Added regex indicators.

--- create_training_data.R
+++ create_training_data.R
@@ -1,14 +1,29 @@
 library(dplyr, warn.conflicts = FALSE)
 library(DBI)
+library(tidyr)
 
-pg <- dbConnect(RPostgres::Postgres())
+pg <- dbConnect(RPostgres::Postgres(), bigint = "integer")
 rs <- dbExecute(pg, "SET search_path TO non_answer, se_features")
 
 fog <- tbl(pg, "fog_measure")
 liwc <- tbl(pg, "liwc")
 word_counts <- tbl(pg, "word_counts")
 
+non_answers_tagged <- tbl(pg, sql("SELECT * FROM non_answer.non_answers_tagged"))
+regexes <- tbl(pg, sql("SELECT * FROM non_answer.regexes"))
 gold_standard <- tbl(pg, sql("SELECT * FROM non_answer.gold_standard"))
+
+regex_matches <-
+    non_answers_tagged %>%
+    mutate(has_non_answer = !is.na(non_answers)) %>%
+    mutate(non_answer = sql("unnest(non_answers)")) %>%
+    mutate(regex_id = sql("(non_answer->'regex_id')::text::integer")) %>%
+    left_join(regexes, by = "regex_id") %>%
+    filter(!is.na(category)) %>%
+    distinct(file_name, answer_nums, regex_id) %>%
+    collect() %>%
+    mutate(value = TRUE, regex_id = sprintf("regex_%02d", regex_id)) %>%
+    spread(regex_id, value, fill = FALSE)
 
 liwc_mod <-
     liwc %>%
@@ -23,15 +38,14 @@ word_counts_mod <-
 fog_mod <-
     fog %>%
     filter(section == 1L, context == "qa") %>%
-    select(-context, -section, -(count:sum_num))
-
+    select(-context, -section)
 
 all_features <-
     liwc_mod %>%
     inner_join(word_counts_mod,
-               by = c("file_name", "last_update", "speaker_number")) %>%
-    inner_join(fog_mod,
-               by = c("file_name", "last_update", "speaker_number"))
+               by = c("file_name", "last_update", "speaker_number"))# %>%
+    #inner_join(fog_mod,
+    #           by = c("file_name", "last_update", "speaker_number"))
 
 matched_features_raw <-
     gold_standard %>%
@@ -43,12 +57,24 @@ matched_features <-
     matched_features_raw %>%
     group_by(file_name, last_update, answer_nums) %>%
     summarize_all(sum, na.rm = TRUE) %>%
-    select(-fog) %>%
+#    select(-fog) %>%
     compute()
 
-training_data <-
+all_data <-
     gold_standard %>%
+    select(-(is_unable:is_after_call)) %>%
+    left_join(matched_features, by = c("file_name", "answer_nums")) %>%
+    collect() %>%
+    mutate(answer_nums = as.character(answer_nums)) %>%
+    left_join(regex_matches, by=c("file_name", "answer_nums")) %>%
+    mutate_at(vars(matches("^regex_")), function(x) coalesce(x, FALSE)) 
+
+training_data <-
+    all_data %>%
     filter(obs_type == "train") %>%
-    select(-obs_type, -(is_unable:is_after_call)) %>%
-    inner_join(matched_features, by = c("file_name", "answer_nums")) %>%
-    collect()
+    select(-obs_type, -last_update)
+
+rs <- dbDisconnect(pg)
+
+objs <- ls()
+rm(list = c(objs[objs != "training_data"], "objs"))
