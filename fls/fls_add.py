265154d955af7abe6cf0c1d12e0d355d00391ce0
Ian Gow <iandgow@gmail.com>
2021-04-10 00:33:10 +1000

    Added initial files for forward-looking statements.

---
+++ fls/fls_add.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pandas as pd
+import os
+import json
+from sqlalchemy import create_engine
+from ling_features import fog
+from sqlalchemy.types import DateTime
+
+conn_string = 'postgresql://' + os.environ['PGHOST'] + '/' + os.environ['PGDATABASE']
+
+def expand_json(df, col):
+    return pd.concat([df.drop([col], axis=1),
+                      df[col].map(lambda x: json.loads(json.dumps(x))).apply(pd.Series)], axis=1)
+
+def add_word_counts(args):
+
+    """Function to get word count data for all utterances in a call"""
+
+    files_w_dates, output_schema, output_table = args[0], args[1], args[2]
+
+    engine = create_engine(conn_string)
+
+    # Get LIWC data. Data is JSON converted to text.
+    for index, file_w_date in files_w_dates.iterrows():
+
+        file_name = file_w_date['file_name']
+        last_update = file_w_date['last_update']
+
+        sql =  """
+        SELECT file_name, last_update, speaker_number, context, section,
+           speaker_text
+        FROM streetevents.speaker_data
+        WHERE file_name = '%s' AND last_update = '%s'
+        """ % (file_name, last_update)
+
+        speaker_data = pd.read_sql(sql, engine)
+        speaker_data['last_update'] = speaker_data['last_update'].map(lambda x: str(x.astimezone('UTC')))
+        speaker_data['fog_data'] = speaker_data['speaker_text'].apply(fog)
+        speaker_data = speaker_data.drop(['speaker_text'], axis=1)
+        
+        # If no speaker_data returned, we still create a DataFrame to keep track
+        # of files that have been processed.
+        if len(speaker_data)==0:
+            d = {'file_name': [file_name], 'last_update': [str(last_update)],
+                 'speaker_number': '0', 'context': 'pres', 'section':1 }
+            speaker_data = pd.DataFrame(d)
+
+        else:
+            # Expand single JSON field to multiple columns
+            speaker_data = expand_json(speaker_data, 'fog_data')
+
+        conn = engine.connect()
+        speaker_data.to_sql(output_table, conn, schema=output_schema, if_exists='append',
+                            index=False,
+                            dtype = {'last_update': DateTime(timezone=True)})
+        conn.close()
+
+    engine.dispose()
---
+++ fls/fls_run.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from multiprocessing import Pool
+from fog_add import add_word_counts
+import pandas as pd
+import os
+from sqlalchemy import create_engine
+import time
+
+output_schema = "se_features"
+output_table  = "fog_measure"
+
+conn_string = 'postgresql://' + os.environ['PGHOST'] + '/' + \
+                os.environ['PGDATABASE']
+
+def getFileNames(output_table, output_schema,
+                                num_files = None):
+
+    engine = create_engine(conn_string)
+
+    # Using LIMIT is much faster than getting all files and ditching
+    # unneeded ones.
+    if num_files==None:
+        limit_clause = ""
+    else:
+        limit_clause = "LIMIT %s" % (num_files)
+
+    # Get a list of unprocessed files. Query differs according to whether
+    # any files have been processed (i.e., output_table exists)
+    table_exists = engine.dialect.has_table(engine, output_table, schema=output_schema)
+    engine.dispose()
+
+    if table_exists:
+        sql = """
+            WITH
+
+            new_files AS (
+            	SELECT file_name, max(last_update) AS last_update
+            	FROM streetevents.calls
+                GROUP BY file_name),
+
+            unprocessed_files AS (
+            	SELECT file_name, last_update FROM new_files
+            	EXCEPT
+            	SELECT file_name, last_update FROM %s.%s)
+
+            SELECT DISTINCT * FROM unprocessed_files
+            %s
+        """ % (output_schema, output_table, limit_clause)
+        engine = create_engine(conn_string)
+        files = pd.read_sql(sql, engine)
+        engine.dispose()
+    else:
+        sql = """CREATE TABLE %s.%s
+                (
+                    file_name text NOT NULL,
+                    last_update timestamp with time zone NOT NULL,
+                    speaker_number integer,
+                    context text NOT NULL,
+                    section integer NOT NULL,
+                    fog float,
+                    complex_words float,
+                    fog_words float,
+                    fog_sents float,
+                    PRIMARY KEY (file_name, last_update, speaker_number, context, section)
+                );
+
+               ALTER TABLE %s.%s OWNER TO %s;
+            """ % (output_schema, output_table,
+                   output_schema, output_table,
+                   output_schema)
+
+        engine = create_engine(conn_string)
+        engine.execute(sql)
+
+        # Create indexes on table.
+        sql = """
+                CREATE INDEX ON %s.%s (file_name);
+                CREATE INDEX ON %s.%s (file_name, last_update);
+            """ % (output_schema, output_table,
+                   output_schema, output_table)
+
+        engine = create_engine(conn_string)
+        engine.execute(sql)
+
+        sql = """
+        	SELECT DISTINCT file_name, max(last_update) AS last_update
+        	FROM streetevents.calls AS a
+        	WHERE event_type=1
+        	GROUP BY file_name
+            %s
+        """ % (limit_clause)
+
+        files = pd.read_sql(sql, engine)
+        engine.dispose()
+
+    return files
+
+# Get a list of files to work on.
+files = getFileNames(output_table, output_schema)
+print("n_files: %d" % len(files))
+
+
+num_threads = 14
+pool = Pool(num_threads)
+
+def chunk_list(l, n):
+    """Yield n successive chunks from l."""
+    size = round(len(l)/n) + 1
+    for i in range(0, len(l), size):
+        yield l[i:i + size]
+
+# Chunk files into num_threads chunks
+files_input = [(files, output_schema, output_table)
+                    for files in chunk_list(files, num_threads)]
+res = pool.map(add_word_counts, files_input)
+
+engine = create_engine(conn_string)
+conn = engine.connect()
+db_comment = "CREATED USING liwc_etc/fog_run.py from " + \
+              "GitHub iangow/se_features ON " + \
+              time.asctime(time.gmtime()) + ' UTC'
+
+conn.execute("COMMENT ON TABLE %s.%s IS '%s'" % (output_schema, output_table, db_comment))
+conn.execute("ALTER TABLE %s.%s OWNER TO %s" % (output_schema, output_table, output_schema))
+conn.execute("GRANT SELECT ON TABLE %s.%s TO %s_access" %
+                (output_schema, output_table, output_schema))
+conn.close()
+engine.dispose()
+
